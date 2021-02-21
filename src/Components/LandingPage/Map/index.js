076c307d509eb0321311eda6f8f527451ef364a0
Ben <Ben@Bens-MacBook-Pro.local>
2021-02-21 00:59:30 -0500

    Map
    Google map is loaded on the page. Need to add much functionality.

--- src/Components/LandingPage/Map/index.js
+++ src/Components/LandingPage/Map/index.js
@@ -1,10 +1,57 @@
-const Map = () => {
+import React from 'react';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
-    return (
-        <div className="Map" style={{ backgroundColor: "lightblue", width: 500, height: 500, margin: 50 }}>
-            <h1>M A P</h1>
-        </div>
-    );
+const containerStyle = {
+    width: '500px',
+    height: '500px',
+    margin: '50px'
+};
+
+const center = {
+    lat: -3.745,
+    lng: -38.523
+};
+
+function MyComponent() {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY
+    });
+
+    const [map, setMap] = React.useState(null)
+
+    // const onLoad = React.useCallback(function callback(map) {
+    //     const bounds = new window.google.maps.LatLngBounds();
+    //     map.fitBounds(bounds);
+    //     setMap(map)
+    // }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerStyle={containerStyle}
+            center={center}
+            zoom={10}
+            // onLoad={onLoad}
+            onLoad={map => {
+                const bounds = new window.google.maps.LatLngBounds();
+                map.fitBounds(bounds);
+            }}
+            onUnmount={onUnmount}
+        >
+            { /* Child components, such as markers, info windows, etc. */ }
+            <></>
+        </GoogleMap>
+    ) : <></>
 }
 
-export default Map;
+export default React.memo(MyComponent);
+
+// return (
+    // <div className="Map" style={{ backgroundColor: "lightblue", width: 500, height: 500, margin: 50 }}>
+    //     <h1>M A P</h1>
+    // </div>
+// );
