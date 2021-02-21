6e1707c1671eb7f7f89c2d05c0daeadd52514a8d
CJ Clark <cj.clark915@gmail.com>
2021-02-20 23:34:14 -0600

    picture and text cards, styled and ready for content

--- src/Components/AboutPage/index.js
+++ src/Components/AboutPage/index.js
@@ -1,8 +1,66 @@
+import Card from 'react-bootstrap/Card';
+import CardColumns from 'react-bootstrap/CardColumns';
+import './about.css';
+
+
+
 const AboutPage = () => {
     return (
         <div className="AboutPage">
-            About Page
+
+        <CardColumns className='col1'>
+        <Card>
+            <Card.Img variant="top" style={{ width: '540px', height: '504' }} src="https://news.wttw.com/sites/default/files/styles/full/public/article/image-non-gallery/CompostProposal_FULL.jpg?itok=QrQHCWZf" />
+        </Card>
+        <Card style={{ width: '540px', height: '504' }}>
+            <Card.Text className="text1" style={{ width: '540px', height: '504' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Temporibus enim cumque nisi veniam aperiam quod, doloremque fugit, 
+                explicabo deserunt, voluptate facere rerum quos quibusdam quisquam rem 
+                qui est obcaecati doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Temporibus enim cumque nisi veniam aperiam quod, doloremque fugit, 
+                explicabo deserunt, voluptate facere rerum quos quibusdam quisquam rem 
+                qui est obcaecati doloribus?
+            </Card.Text>
+        </Card>
+        </CardColumns>
+
+        <CardColumns className='col2'>
+        <Card>
+            <Card.Img variant="top" style={{ width: '540px', height: '504' }} src="https://news.wttw.com/sites/default/files/styles/full/public/article/image-non-gallery/CompostProposal_FULL.jpg?itok=QrQHCWZf" />
+        </Card>
+        <Card style={{ width: '540px', height: '504' }}>
+            <Card.Text className="text1" style={{ width: '540px', height: '504' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Temporibus enim cumque nisi veniam aperiam quod, doloremque fugit, 
+                explicabo deserunt, voluptate facere rerum quos quibusdam quisquam rem 
+                qui est obcaecati doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Temporibus enim cumque nisi veniam aperiam quod, doloremque fugit, 
+                explicabo deserunt, voluptate facere rerum quos quibusdam quisquam rem 
+                qui est obcaecati doloribus?
+            </Card.Text>
+        </Card>
+        </CardColumns>
+
+        <CardColumns className='col3'>
+        <Card>
+            <Card.Img variant="top" style={{ width: '540px', height: '504' }} src="https://news.wttw.com/sites/default/files/styles/full/public/article/image-non-gallery/CompostProposal_FULL.jpg?itok=QrQHCWZf" />
+        </Card>
+        <Card style={{ width: '540px', height: '504' }}>
+            <Card.Text className="text1" style={{ width: '540px', height: '504' }}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Temporibus enim cumque nisi veniam aperiam quod, doloremque fugit, 
+                explicabo deserunt, voluptate facere rerum quos quibusdam quisquam rem 
+                qui est obcaecati doloribus? Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                Temporibus enim cumque nisi veniam aperiam quod, doloremque fugit, 
+                explicabo deserunt, voluptate facere rerum quos quibusdam quisquam rem 
+                qui est obcaecati doloribus?
+            </Card.Text>
+        </Card>
+        </CardColumns>
+
         </div>
+        
     )
 }
 
